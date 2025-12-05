415f722bc9338a0d6f11ca2ee96dc1d7bf4f599c
mohan kumar <monukashyap7895@gmail.com>
2025-12-05 22:27:31 +0530

    chnages

--- Route/index.js
+++ Route/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 const routes = express.Router();
 import auth from './auth.js'
 import user from './user.js'
+import home from './home.js'
+
+routes.use('/home',home)
 
 routes.use('/auth',auth)
 routes.use('/user',user)
